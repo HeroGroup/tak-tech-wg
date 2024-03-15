878703f1fdd1a55e607dd8028953c5c45abd7b80
navid hero <navid_hero_1st@yahoo.com>
2024-03-15 09:40:31 +0330

    change pagination location

--- resources/views/admin/WGPeers.blade.php
+++ resources/views/admin/WGPeers.blade.php
@@ -153,8 +153,9 @@
     @endforeach
     </tbody>
   </table>
+  {{ $peers->links() }}
 </div>
-{{ $peers->links() }}
+
 
 <!-- Edit peers Mass Modal -->
 <div class="modal fade" id="edit-peers-mass-modal" tabindex="-1" role="dialog" aria-labelledby="editPeersMassModalLabel" aria-hidden="true">
