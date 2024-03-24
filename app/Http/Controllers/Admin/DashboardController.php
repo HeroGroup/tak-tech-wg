c03de3ae68e3013add55cedee8caa1de5f98f437
navid hero <navid_hero_1st@yahoo.com>
2024-03-24 15:28:45 +0330

    seperate limited and unlimited peers count

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -6,23 +6,31 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+// This class has some statistical methods to show to user
 class DashboardController extends Controller
 {
+    // This function return a few statistics to show on diagrams
     public function dashboard()
     {   
         $numberOfServers = DB::table('servers')->count();
-        if (auth()->user()->is_admin) {
-            $numberOfInterfaces = DB::table('interfaces')->count();
-            $numberOfPeers = DB::table('peers')->count();
-        } else {
-            $numberOfInterfaces = DB::table('user_interfaces')->where('user_id', auth()->user()->id)->count();
-            $numberOfPeers = DB::table('user_interfaces')
-                ->where('user_id', auth()->user()->id)
-                ->join('peers', 'user_interfaces.interface_id', '=', 'peers.interface_id')
-                ->count();
-        }
-        $numberOfLimitedPeers = 0;
-
-        return view('admin.dashboard', compact('numberOfServers', 'numberOfInterfaces', 'numberOfPeers', 'numberOfLimitedPeers'));
+        
+        $numberOfInterfaces = DB::table('user_interfaces')->where('user_id', auth()->user()->id)->count();
+        
+        $numberOfUnlimitedPeers = DB::table('user_interfaces')
+            ->where('user_id', auth()->user()->id)
+            ->join('interfaces', 'user_interfaces.interface_id', '=', 'interfaces.id')
+            ->join('peers', 'interfaces.id', '=', 'peers.interface_id')
+            ->where('interfaces.iType', 'unlimited')
+            ->count();
+            
+        
+        $numberOfLimitedPeers = DB::table('user_interfaces')
+            ->where('user_id', auth()->user()->id)
+            ->join('interfaces', 'user_interfaces.interface_id', '=', 'interfaces.id')
+            ->join('peers', 'interfaces.id', '=', 'peers.interface_id')
+            ->where('interfaces.iType', 'limited')
+            ->count();
+        
+        return view('admin.dashboard', compact('numberOfServers', 'numberOfInterfaces', 'numberOfUnlimitedPeers', 'numberOfLimitedPeers'));
     }
 }
