a6fe8a3c5971adc9be00a57990f750b678d92770
navid hero <navid_hero_1st@yahoo.com>
2024-03-28 22:36:31 +0330

    full row

--- resources/views/admin/interfaces/usageDetails.blade.php
+++ resources/views/admin/interfaces/usageDetails.blade.php
@@ -2,18 +2,12 @@
 @section('content')
 <div class="row">
   @foreach($output as $server => $value)
-  <div class="col-lg-6">
+  <div class="col-lg-12">
         <div class="card shadow mb-4">
             <!-- Card Header - Dropdown -->
             <div
                 class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                 <h6 class="m-0 font-weight-bold text-primary">{{$server}}</h6>
-                <div class="dropdown no-arrow">
-                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                    </a>
-                </div>
             </div>
             <!-- Card Body -->
             <div class="card-body">
